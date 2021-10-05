d61caced4918277015c7697bd52034bdf29362f8
rokibulslame <rokibulslam.programmer@gmail.com>
2021-10-05 21:14:55 +0600

    btn color

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -21,7 +21,7 @@ const Banner = () => {
             <img src={img} className="rounded mx-2 img-fluid pt-5 " alt="" />
             <h1 className="mt-5 fw-light fs-2 "> Learn Without Limits</h1>
             <Link to="/login">
-              <button className="btn-primary btn  fs-4  fw-light mx-3 mb-5">Join For Free</button>
+              <button className="btn-color rounded-3  fs-4  fw-light mx-3 mb-5">Join For Free</button>
             </Link>
             </div>
         </div>
